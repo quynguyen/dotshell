eb350f29a8306e24012bed416ee75bc5e0e85714
quy.nguyen <quy.nguyen@gmail.com>
2016-12-01 17:03:41 -0800

    Simplied structure

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo --------------------------------------------------------
-echo The fullpath to here is:
-echo --------------------------------------------------------
+# Change to the directory where the script lives
+# So that everything is relative to this script directory.
 pushd `dirname $0` > /dev/null
 HERE=`pwd`
 popd > /dev/null
+
+
+echo --------------------------------------------------------
+echo The fullpath to here is:
+echo --------------------------------------------------------
 echo $HERE
 
 echo --------------------------------------------------------
@@ -15,6 +19,9 @@ git submodule init
 git submodule update
 git submodule foreach --recursive git submodule update --init
 
+# Source in some core.sh
+source ./config/shell-config/common/core.sh
+
 echo --------------------------------------------------------
 echo Symlinking Home Dotfiles
 echo --------------------------------------------------------
@@ -22,39 +29,41 @@ for file in `ls ./dotfiles`; do
 	#eg "config" to ".config"#
 	sourceFile=$HERE/dotfiles/$file
 	dotFile="$HOME/.${file}"
-	renamedDotFile="${dotFile}-$(date +%^s)"
+#	renamedDotFile="${dotFile}-$(date +%^s)"
+
+	relink $sourceFile $dotFile
 
 	#Is the dotFile a directory that already exists
 	#.. and dotFile isn't pointing to the same location to the sourceFile
-	if [ -d $dotFile ] && [ ! $dotFile -ef $sourceFile ]; then
-		echo -----------------------------------------
-		echo "Renaming ${dotFile} to ${renamedDotFile}"
-		echo -----------------------------------------
-		#rename it
-		mv -v $dotFile $renamedDotFile
-	fi
-
-	#Create the symlinks in $HOME
-	ln -vsnf $sourceFile $dotFile; 
-
-	#Move over any existing files
-	if [ -d $renamedDotFile ]; then
-		echo -----------------------------------------
-		echo "Moving files from ${renamedDotFile} to ${dotFile}" 
-		echo -----------------------------------------
-		mv -v $renamedDotFile/* $dotFile/
-		rm -vrf $renamedDotFile
-		unset renamedDotFile
-	fi
+#	if [ -d $dotFile ] && [ ! $dotFile -ef $sourceFile ]; then
+#		echo -----------------------------------------
+#		echo "Renaming ${dotFile} to ${renamedDotFile}"
+#		echo -----------------------------------------
+#		#rename it
+#		mv -v $dotFile $renamedDotFile
+#	fi
+#
+#	#Create the symlinks in $HOME
+#	ln -vsnf $sourceFile $dotFile; 
+#
+#	#Move over any existing files
+#	if [ -d $renamedDotFile ]; then
+#		echo -----------------------------------------
+#		echo "Moving files from ${renamedDotFile} to ${dotFile}"
+#		echo -----------------------------------------
+#		mv -v $renamedDotFile/* $dotFile/
+#		rm -vrf $renamedDotFile
+#		unset renamedDotFile
+#	fi
 done
 
 
 echo --------------------------------------------------------
-echo Handling Overlays
+echo Overlaying Custom Configurations into their existing locations
 echo --------------------------------------------------------
 for overlay in `find $HERE/overlays/ -mindepth 1  -type d -printf "%f\n"`; do
 	for link in `ls $HERE/overlays/$overlay`; do
-		ln -vsnf $HERE/overlays/$overlay/$link $HOME/$overlay/$link
+		relink $HERE/overlays/$overlay/$link $HOME/$overlay/$link
 	done
 done
 
