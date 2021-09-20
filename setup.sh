80a7f77a875e5f5c606b87385b55ed76072e3867
quynguyen <quy.nguyen@gmail.com>
2021-09-20 12:43:31 -0700

    Force Commit

--- setup.sh
+++ setup.sh
@@ -122,7 +122,6 @@ echo --------------------------------------------------------
 zsh_lines=(
 	"export ENV=$ENV"
 	"source $ENV/.submodules/shell-config/zsh/profile"
-	"eval \"\$(rbenv init -)\""
 	"#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!"
 	"export SDKMAN_DIR=\"\$HOME/.sdkman\""
 	"[[ -s \"\$HOME/.sdkman/bin/sdkman-init.sh\" ]] && source \"\$HOME/.sdkman/bin/sdkman-init.sh\""
